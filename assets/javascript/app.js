b13a914ef9ea049b3b6fd5eda89741419ea7b400
lileu <lis.leu@gmail.com>
2019-07-17 20:10:11 +1000

    added pseudocode skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+
+// VARIABLES
+// ==========================================================================
+// question object/properties/values
+var questions = {
+    
+}
+
+// game variables and methods
+var game = {
+    
+
+    
+    
+    // start game 
+    startGame: function() {
+        
+    },
+    
+    // player clicks 
+    checkGuess: function() {
+        
+    },
+    // correct guess
+    correctAnswer: function() {
+        
+    },
+    // incorrect guess
+    incorrectAnswer: function() {
+        
+    },
+    
+    // next question
+    nextQuestion: function() {
+
+    },
+    
+     // time out game 
+     // show correct count, incorrect count, unanswer? option to restart by clicking
+     timeOut: function() {
+         
+    },
+    
+    // reset game 
+    resetGame: function() {
+        
+    },    
+}
+
+// MAIN PROCESS
+// ==========================================================================
+// $(document).ready(function(){
+
+
+// })
